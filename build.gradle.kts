5361aecfca623c345c3f00ed862cb2acda037619
Max Mishenko <kirova32a@mail.ru>
2025-01-26 19:46:55 +0600

    [~] fix web hook

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,12 @@
+import java.io.FileInputStream
+import java.util.*
+
+val localProperty =
+    Properties().apply { load(FileInputStream(rootProject.file("local.properties"))) }
+
 plugins {
     kotlin("jvm") version "2.1.0"
+    id("io.ktor.plugin") version "3.0.3"
 }
 
 group = "ru.mishenko.maksim"
@@ -22,7 +29,7 @@ kotlin {
 }
 
 dependencies {
-    implementation ("io.github.kotlin-telegram-bot.kotlin-telegram-bot:telegram:6.2.0")
+    implementation("io.github.kotlin-telegram-bot.kotlin-telegram-bot:telegram:6.2.0")
     api("tech.ydb:ydb-sdk-bom:2.1.11")
     api("tech.ydb:ydb-sdk-table:2.1.11")
     api("tech.ydb:ydb-sdk-topic:2.1.11")
@@ -30,10 +37,32 @@ dependencies {
     api("tech.ydb:ydb-sdk-coordination:2.1.11")
     api("tech.ydb.auth:yc-auth-provider:2.1.1")
     api("io.perfmark:perfmark-api:0.27.0")
+    implementation("io.ktor:ktor-server-core:3.0.3")
+    implementation("io.ktor:ktor-server-cio:3.0.3")
+
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
     kotlinOptions {
         freeCompilerArgs = freeCompilerArgs + "-Xcontext-receivers"
+    }
+}
+
+ktor {
+    docker {
+        jib {
+            from {
+                image = "openjdk:17-jdk"
+            }
+            to {
+                val dockerHubUrl = localProperty.getProperty("yandex.registry.url") as String
+                val yandexToken = localProperty.getProperty("yandex.oauth.token") as String
+                image = "$dockerHubUrl/bot"
+                auth {
+                    username = "oauth"
+                    password = yandexToken
+                }
+            }
+        }
     }
 }
--- src/main/kotlin/bot/botWebhook.kt
+++ src/main/kotlin/bot/botWebhook.kt
@@ -2,21 +2,41 @@ package bot
 
 import com.github.kotlintelegrambot.Bot
 import com.github.kotlintelegrambot.webhook
+import io.ktor.http.*
+import io.ktor.server.cio.*
+import io.ktor.server.engine.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 import resources.R
 
-fun Bot.Builder.setupWebHook(){
+fun Bot.Builder.setupWebHook() {
     val url = R.webHookUrl
-    if (url != null){
+    if (url != null) {
         webhook {
-            this.url = url
+            this.url = "$url/${R.botToken}"
+            maxConnections = 50
+            allowedUpdates = listOf("message")
         }
     }
 }
 
-fun Bot.start(){
-    if (R.webHookUrl == null){
+fun Bot.start() {
+    if (R.webHookUrl == null) {
         startPolling()
     } else {
         startWebhook()
+        embeddedServer(CIO, host = "0.0.0.0", port = 8080) {
+            routing {
+                post("/${R.botToken}") {
+                    val response = call.receiveText()
+                    processUpdate(response)
+                    call.respond(HttpStatusCode.OK)
+                }
+                get("/start") {
+                    call.respondText("Hello world")
+                }
+            }
+        }.start(wait = true)
     }
 }
